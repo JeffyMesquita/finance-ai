f3a1d8f50e5dc781bf6d93d8e9e2d9a387ab37bd
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2024-11-13 21:17:54 -0300

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
+
+export default function LoginPage() {
+  return (
+    <main className="grid h-full md:grid-cols-2">
+      {/* Left side */}
+
+      <section className="mx-auto flex h-full max-w-[500px] flex-col justify-center gap-8 p-8">
+        <Image src="/logo.svg" alt="Finance AI" height={39} width={173} />
+        <article className="flex flex-col gap-3">
+          <h1 className="text-4xl font-bold">Bem-vindo</h1>
+          <p className="text-muted-foreground">
+            A Finance AI é uma plataforma de gestão financeira que utiliza IA
+            para monitorar suas movimentações, e oferecer insights
+            personalizados, facilitando o controle do seu orçamento.
+          </p>
+        </article>
+        <Button variant="outline">
+          <LogInIcon className="mr-2 h-6 w-6" />
+          Fazer login ou criar conta
+        </Button>
+      </section>
+      {/* Right side */}
+      <section className="relative hidden h-full w-full md:flex">
+        <Image
+          src="/login.png"
+          alt="Faça Login"
+          fill
+          className="object-cover"
+        />
+      </section>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
-  return (
-    <main className="flex w-screen items-center justify-center">
-      <h1 className="p-5 text-red-500">Finance AI</h1>
-    </main>
-  );
+  return <Button>Hello World!</Button>;
 }
