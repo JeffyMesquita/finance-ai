10b1082949aea7ef0a386a98e9c7b1405dcb2e74
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2024-11-13 21:30:16 -0300

    feat: signIn using clerk auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const { userId } = await auth();
+
+  if (userId) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   return (
     <main className="grid h-full md:grid-cols-2">
       {/* Left side */}
-
       <section className="mx-auto flex h-full max-w-[500px] flex-col justify-center gap-8 p-8">
         <Image src="/logo.svg" alt="Finance AI" height={39} width={173} />
         <article className="flex flex-col gap-3">
@@ -17,11 +29,14 @@ export default function LoginPage() {
             personalizados, facilitando o controle do seu orçamento.
           </p>
         </article>
-        <Button variant="outline">
-          <LogInIcon className="mr-2 h-6 w-6" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2 h-6 w-6" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </section>
+
       {/* Right side */}
       <section className="relative hidden h-full w-full md:flex">
         <Image
